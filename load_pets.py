f35306861ab27978185f269e517b7a653f21fa90
Audodido <chanwick@gmail.com>
2021-11-06 13:01:16 -0400

    s

--- load_pets.py
+++ load_pets.py
@@ -1,6 +1,10 @@
 import sqlite3
 
-print("hello")
+conn = sqlite3.connect('pets.db') #connect to the database
+
+cur = conn.cursor() #once you have a cursor you can start executing sql commnands
+
+
 
 if __name__ == "__main__":
     pass
